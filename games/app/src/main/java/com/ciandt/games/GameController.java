08f22a1eb5af8d886aa8126398d827da0a5ef3a0
Christian Segala <csegala@ciandt.com>
2021-04-28 18:09:51 -0300

    cors all

--- games/app/src/main/java/com/ciandt/games/GameController.java
+++ games/app/src/main/java/com/ciandt/games/GameController.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
 
-@CrossOrigin(origins = { "amazonaws.com", "localhost" })
+@CrossOrigin(CorsConfiguration.ALL)
 @RestController
 public class GameController {
     @Autowired
